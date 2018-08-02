2eb5ed6f18647e87d426b3c822e51028765ed907
Jim Merioles <jimwisleymerioles@gmail.com>
2018-08-02 08:52:24 +0800

    Configure database for Heroku.

--- config/database.php
+++ config/database.php
@@ -31,6 +31,12 @@ return [
     |
     */
 
+    $url = parse_url(env("DATABASE_URL"));
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+
     'connections' => [
 
         'sqlite' => [
@@ -56,11 +62,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', $url['host']),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', substr($url['path'], 1)),
+            'username' => env('DB_USERNAME', $url['user']),
+            'password' => env('DB_PASSWORD', $url['pass']),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
